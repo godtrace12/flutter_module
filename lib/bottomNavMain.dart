c765dc89ae3e894932c99f6cc6d48479b176c0bc
daijun <595756608@qq.com>
2019-07-24 11:23:25 +0800

    [add] 通过scrollControler判断滑动到列表末尾来进行上拉加载。

--- lib/bottomNavMain.dart
+++ lib/bottomNavMain.dart
@@ -3,6 +3,7 @@ import 'devicevideo/devicelist/infiniteWordList.dart';
 import 'devicevideo/devicelist/container.dart';
 import 'devicevideo/devicelist/noSeparateWordList.dart';
 import 'devicevideo/devicelist/refreshWordList.dart';
+import 'devicevideo/devicelist/refreshScrollWordList.dart';
 
 class BottomNavMain extends StatefulWidget{
   @override
@@ -26,7 +27,7 @@ class _BottomNavMainState extends State<BottomNavMain>{
   List<MainItem> mainItems = <MainItem>[
     new MainItem(title: '视频',icon: Icons.directions_car,widget:new InfiniteWordList()),
     new MainItem(title: '消息', icon: Icons.directions_bike,widget:new RefreshWordList()),
-    new MainItem(title: '工单', icon: Icons.directions_boat,widget:new ContainerDemo()),
+    new MainItem(title: '工单', icon: Icons.directions_boat,widget:new RefreshScrollWordList()),
     new MainItem(title: '直播', icon: Icons.directions_bus,widget:new ContainerDemo()),
     new MainItem(title: '个人中心', icon: Icons.directions_railway,widget:new ContainerDemo()),
   ];
@@ -49,7 +50,7 @@ class _BottomNavMainState extends State<BottomNavMain>{
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-      appBar: new AppBar(title: new Text("bottom navigation"),),
+      appBar: new AppBar(title: new Text('${mainItems[_currentIndex].title}')),
       body: IndexedStack(
         index: _currentIndex,
         children: _navTabViews,
---
+++ lib/devicevideo/devicelist/refreshScrollWordList.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'adapter/devListAdapter.dart';
+
+//下拉刷新，上拉加载更多列表
+class RefreshScrollWordList extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    print("dj refresh create");
+    return new RefreshScrollwordListState();
+  }
+
+}
+
+
+class RefreshScrollwordListState extends State<RefreshScrollWordList>{
+  var _words = <String>[];
+  ScrollController _scrollController = ScrollController();
+  bool isFirstTime = true;
+
+  @override
+  void initState(){
+    super.initState();
+    print("dj refresh initState");
+    if(isFirstTime){
+      _retrieveData();
+    }
+    //列表滑动到末尾，并且长度还不足15时去加载更多
+    _scrollController.addListener((){
+      if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent
+        && _words.length <15){
+        print("滑动到了底部，准备加载更多");
+        _retrieveData();
+      }
+    });
+  }
+
+  @override
+  void dispose(){
+    super.dispose();
+    _scrollController.dispose();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+  // 下拉刷新样式
+    return new RefreshIndicator(
+        child: new ListView.builder(
+            controller: _scrollController,
+            itemBuilder: (context,index){
+//          //表尾
+          if(index == _words.length) {
+            if(_words.length <15){
+              // 不足100，继续获取数据
+              //加载显示loading
+              return Container(
+                padding: const EdgeInsets.all(16.0),
+                alignment: Alignment.center,
+                child: SizedBox(
+                    width: 24.0,
+                    height: 24.0,
+                    child: CircularProgressIndicator(strokeWidth: 2.0)),
+              );
+            }else{
+              return Container(alignment: Alignment.center,
+              padding: EdgeInsets.all(16.0),
+              child: Text("没有更多了",style: TextStyle(color: Colors.grey),),);
+            }
+          }
+          // 显示设备列表适配器
+          return DevListAdapter(_words[index]);
+        },
+//            长度为实际数据长度加1，这个1用来显示上拉加载中或没有更多
+        itemCount: _words.length+1),
+        onRefresh: _onRrefresh);
+  }
+
+
+  void _retrieveData(){
+    print("dj  retrieveData");
+    Future.delayed(Duration(seconds: 2)).then((e){
+      print("dj retrieveData currentLength=${_words.length}");
+      //每次生成20单词，单词插入到最后一个单词到前面
+      _words.addAll(generateWordPairs().take(5).map((e)=>e.asPascalCase+'${_words.length}').toList());
+
+      setState(() {
+
+      });
+    });
+
+  }
+
+  Future<Null> _onRrefresh() async{
+    await Future.delayed(Duration(seconds: 2),(){
+      _words.clear();
+      _words.addAll(generateWordPairs().take(5).map((e)=>e.asPascalCase+'${_words.length}').toList());
+      setState(() {
+
+      });
+    });
+  }
+
+
+
+}
+
